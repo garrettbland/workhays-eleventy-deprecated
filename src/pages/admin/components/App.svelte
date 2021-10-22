cad109ed175462ec6a627668c1fc5bd2cb96037c
garrettbland <gmorganbland@gmail.com>
2021-10-21 22:32:25 -0500

    setup local storage for conditional top bar links

--- src/pages/admin/components/App.svelte
+++ src/pages/admin/components/App.svelte
@@ -7,31 +7,55 @@
         user_email,
         user_email_verified,
     } from '../stores'
-
-    /**
-     * Initialize Firebase and check authorization
-     */
-    //import { firebaseapp } from '../firebase'
-    // import { onAuthStateChanged } from '@firebase/auth'
     import { onAuthStateChanged } from 'firebase/auth'
     import { auth } from '../firebase'
     import Loading from './Loading'
 
     let is_loading = true
 
+    /**
+     * Check if local storage key exists. If it doesn't, then
+     * create it. We use this to display "Sign in" or "Dashboard"
+     * in navbar.liquid
+     */
+    onMount(() => {
+        if (!localStorage.getItem('is_user_authenticated') === null) {
+            localStorage.setItem('is_user_authenticated', 'false')
+        } else {
+            console.log(localStorage.getItem('is_user_authenticated'))
+        }
+    })
+
+    const set_user_local_storage = (user_state) => {
+        /**
+         * Set local storage item so we can display 'sign in'
+         * or 'dashboard' in navbar.liquid.
+         *
+         * We have to manually dispatch the storage event, because
+         * the 'storage' event only gets picked up by the listener
+         * if localStorage was changed in another browsers tab/window
+         */
+        localStorage.setItem('is_user_authenticated', user_state)
+        dispatchEvent(new Event('storage'))
+    }
+
     onAuthStateChanged(auth, (user) => {
-        // check on user status
-        console.log('checking user...')
         if (user) {
+            /**
+             * User is signed in
+             */
             is_loading = false
-            console.log('setting logged in status to true...')
             is_authenticated.set(true)
             user_email.set(user.email)
             user_email_verified.set(user.emailVerified)
+            set_user_local_storage(true)
         } else {
+            /**
+             * User is not signed in
+             */
             is_loading = false
-            console.log('setting logged in status to false...')
             is_authenticated.set(false)
+            set_user_local_storage(false)
         }
     })
 
@@ -52,6 +76,7 @@
     import NotFound from '../routes/NotFound'
     import SignIn from '../routes/SignIn'
     import Register from '../routes/Register'
+    import { onMount } from 'svelte'
 
     const authenticated_routes = {
         '/': Dashboard,
